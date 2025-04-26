b459859e1fb20341824168cec336d469155f9aa3
Ic1558 <ittipong.c@gmail.com>
2025-04-27 01:34:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseUpload
+import io, os
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from Render + FastAPI"}
+SERVICE_ACCOUNT_JSON = "service_account.json"
+FOLDER_ID = os.getenv("GDRIVE_FOLDER_ID")
+
+creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_JSON)
+drive_service = build('drive', 'v3', credentials=creds)
+
+@app.post("/upload")
+async def upload_to_drive(file: UploadFile = File(...)):
+    file_content = await file.read()
+    media = MediaIoBaseUpload(io.BytesIO(file_content), mim_
